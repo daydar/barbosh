30bb084e84ac9e1e99bb0b7b79347ce2b0100130
Deniz Aydar <deniz.aydar@gmx.de>
2023-08-27 22:20:58 +0200

    fix: logging

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ client.on('ready', async () => {
 				// Loop the audio with a delay using setTimeout
 				setTimeout(() => {
 					playSong();
-					console.log(`The time is ${date.toLocaleDateString("de-DE")}`);
+					console.log(`The time is ${date.toLocaleDateString(undefined, {timeStyle: "medium", dateStyle: "medium"})}`);
 				}, 5000);
 			}
 		});
